5deaa49c3a6f7ca8bf23e381ee79f5baf2f888e2
miigotu <miigotu@gmail.com>
2023-06-21 04:24:41 -0400

    Update sickchill.rb

--- Formula/sickchill.rb
+++ Formula/sickchill.rb
@@ -1,16 +1,16 @@
 class Sickchill < Formula
   include Language::Python::Virtualenv
 
-  desc "SickChill packaged into a homebrew bottle with a service"
+  desc "Media library mega tool, packaged into a homebrew bottle with a service"
   homepage "https://sickchill.github.io"
   url "https://files.pythonhosted.org/packages/ba/75/d27b26bdac593423026a8d606551de134f745fb36ec991ff0a70d4dfb2a0/sickchill-2023.5.30.tar.gz"
   sha256 "be49f0d6c01f479cb0f066e88582c4971b683a11d50149ffe81b1bdc4447c2ab"
 
-  depends_on "python3"
   depends_on "rust" => :build
   depends_on "cffi"
   depends_on "pycparser"
-  depends_on "pyyaml"
+  depends_on "python3"
+  depends_on "pyyaml
 
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
